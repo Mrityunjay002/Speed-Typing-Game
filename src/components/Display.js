6c2440cfbf24bde7bebdfe7299d25d7896079979
Mrityunjay002 <kumarmrityunjay002@gmail.com>
2021-10-20 12:51:00 +0530

    ..

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Display() {
   const [text, settext] = useState("");
   const [timeLeft, settimeLeft] = useState(2);
   const [isStart, setisStart] = useState(false);
-  const [word,setWord]=useState(0)
+  const [word, setWord] = useState(0);
+  const TypingRef = useRef(null)
 
   useEffect(() => {
     if (timeLeft > 0 && isStart) {
@@ -12,31 +13,40 @@ function Display() {
         settimeLeft(timeLeft - 1);
       }, 1000);
     } else if (timeLeft === 0) {
-       
-      setisStart(false);
-      setWord(countWords(text))
+      endGame()
     }
   }, [timeLeft, isStart]);
 
   const handleChange = (e) => {
     settext(e.target.value);
   };
+  function startClock(){
+    setisStart(true)
+    settimeLeft(5)
+    settext("")
+    setWord(0)
+    TypingRef.current.disabled=false
+    TypingRef.current.focus()
+  }
+  function endGame(){
+    setisStart(false);
+    setWord(countWords(text));
+
+  }
   function countWords(text) {
-    setisStart(true);
+   
 
     const wordsArr = text.trim().split(" ");
-    
+
     return wordsArr.filter((word) => word !== "").length;
   }
 
   return (
     <div>
       <h1>How fast do you type..??</h1>
-      <textarea onChange={handleChange} />
+      <textarea ref={TypingRef} disabled={!isStart} value={text} onChange={handleChange} />
       <h4>Time Remaining : {timeLeft}</h4>
-      <button onClick={()=>setisStart(true)}>
-        Start
-      </button>
+      <button disabled={isStart} onClick={startClock}>Start</button>
       <h4>Word : {word}</h4>
     </div>
   );
